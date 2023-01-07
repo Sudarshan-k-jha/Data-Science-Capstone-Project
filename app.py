1c5dd4f11864ac54b518e176778a2fb0f627bccb
Sudarshan-k-jha <121251733+Sudarshan-k-jha@users.noreply.github.com>
2023-01-07 13:28:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,32 +19,36 @@ def predict():
     if request.method == 'POST':
         Year = int(request.form['year'])
         selling_price=float(request.form['selling_price'])
-        Kms_Driven=int(request.form['Kms_Driven'])
-        Kms_Driven2=np.log(Kms_Driven)
-        Owner=int(request.form['Owner'])
+        km_driven=float(request.form['km_driven'])
+        owner_First Owner=request.form['owner_First Owner']
+        if(owner_First Owner=='First owner'):
+            owner_First Owner=0
+            owner_Second Owner=2
+            owner_Third Owner=3
+            owner_Fourth & Above Owner=1
+         else:
+            owner_Test Drive Car=4
         fuel_Petrol=request.form['fuel_Petrol']
         if(fuel_Petrol=='Petrol'):
                 fuel_Petrol=4
                 fuel_Diesel=1
                 fuel_CNG=0
-                fuel_LPG=3
-                fuel_Electric=2
-                
+                fuel_LPG=3      
         else:
-            Fuel_Type_Petrol=0
-            Fuel_Type_Diesel=1
-        Year=2020-Year
-        Seller_Type_Individual=request.form['Seller_Type_Individual']
-        if(Seller_Type_Individual=='Individual'):
-            Seller_Type_Individual=1
+            fuel_Electric=2
+        Year=2023-year
+        seller_type_Individual=request.form['seller_type_Individual']
+        if(seller_type_Individual=='Individual'):
+            seller_type_Individual=1
+            seller_type_Dealer=0
         else:
-            Seller_Type_Individual=0	
-        Transmission_Mannual=request.form['Transmission_Mannual']
-        if(Transmission_Mannual=='Mannual'):
-            Transmission_Mannual=1
+            seller_type_Trustmark Dealer=2	
+        transmission_Manual=request.form['transmission_Mannual']
+        if(transmission_Manual=='Manual'):
+            transmission_Manual=1
         else:
-            Transmission_Mannual=0
-        prediction=model.predict([[Present_Price,Kms_Driven2,Owner,Year,Fuel_Type_Diesel,Fuel_Type_Petrol,Seller_Type_Individual,Transmission_Mannual]])
+            transmission_Automatic=0
+        prediction=model.predict([[selling_price,km_driven2,owner_First Owner,owner_Second Owner,owner_Third Owner,owner_Fourth & Above Owner,owner_Test Drive Cars,Year,fuel_Diesel,fuel_Petrol,fuel_CNG,fuel_LPG,fuel_Electric,seller_type_Individual,seller_type_Dealer,seller_type_Trustmark Dealer,transmission_Manual,transmittion_Automatic]])
         output=round(prediction[0],2)
         if output<0:
             return render_template('index.html',prediction_texts="Sorry you cannot sell this car")
