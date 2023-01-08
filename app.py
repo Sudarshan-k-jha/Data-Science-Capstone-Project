a549b2e6379c5fee2f31cea3eef8eddc4becbd56
Sudarshan-k-jha <121251733+Sudarshan-k-jha@users.noreply.github.com>
2023-01-08 15:09:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,16 +5,7 @@ import pickle
 import numpy as np
 import sklearn
 from sklearn.preprocessing import StandardScaler
-app = Flask(__name__)
 model = pickle.load(open('dtr.pkl', 'rb'))
-@app.route('/',methods=['GET'])
-def Home():
-    return render_template('index.html')
-
-
-standard_to = StandardScaler()
-@app.route("/predict", methods=['POST'])
-def predict():
     Fuel_Type_Diesel=1
     if request.method == 'POST':
         Year = int(request.form['year'])
